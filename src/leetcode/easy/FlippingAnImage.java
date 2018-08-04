772922bc6268729f75cfff1c5538eb3e1ab1a11d
hefangzhou@vpgame.cn <hefangzhou@vpgame.cn>
2018-08-04 14:41:06 +0800

    FlippingAnImage 优化

--- src/leetcode/easy/FlippingAnImage.java
+++ src/leetcode/easy/FlippingAnImage.java
@@ -1,29 +1,37 @@
 package leetcode.easy;
 
 /**
- * @link https://leetcode-cn.com/problems/flipping-an-image/description/
- *
  * @author <a href="mailto:hefangzhou@vpgame.cn">hefangzhou</a>
+ *
+ * @link https://leetcode-cn.com/problems/flipping-an-image/description/
  * @date 2018/8/4 下午2:16
  */
 public class FlippingAnImage {
 
-    public int[][] flipAndInvertImage(int[][] A) {
-        int[][] B = A;
+    public static int[][] flipAndInvertImage(int[][] A) {
         for (int i = 0; i < A.length; i++) {
             int[] temp = new int[A[i].length];
             for (int j = 0; j < temp.length; j++) {
                 temp[temp.length - 1 - j] = A[i][j];
             }
-            B[i] = temp;
+            A[i] = temp;
         }
 
-        for (int i = 0; i < B.length; i++) {
-            for (int j = 0; j < B[i].length; j++) {
-                B[i][j] = B[i][j] ^ 1;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                A[i][j] = A[i][j] ^ 1;
             }
         }
-        return B;
+        return A;
+    }
+
+    public static void main(String[] args) {
+        int[][] A = new int[][]{
+                {1, 1, 0},
+                {1, 0, 1},
+                {0, 0, 0}
+        };
+        flipAndInvertImage(A);
     }
 
 }
